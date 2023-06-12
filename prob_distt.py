ab13d45c260cc2d2fc0076ae1b19661dca174ca9
aisyakmila <130299353+aisyakmila@users.noreply.github.com>
2023-06-13 04:37:10 +0700

    Update prob_distt.py

--- prob_distt.py
+++ prob_distt.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 from scipy import stats
-from streamlit_option_menu import option_menu
+import streamlit_option_menu as option_menu
 
 with st.sidebar:
     selected = option_menu(
